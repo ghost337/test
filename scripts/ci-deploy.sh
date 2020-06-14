57a01e87b8558b1467e010e948b615cf55335c27
Ivan <klimov.i.a@gmail.com>
2020-06-14 20:53:47 +0300

    second commit

--- scripts/ci-deploy.sh
+++ scripts/ci-deploy.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
+# exit script when any command ran here returns with non-zero exit code
+set -e
 
 echo "hello world 123!"
+
+export mykey="$mykey"
+
+envsubst <./docker-compose.yml >./docker-compose.yml.out
+mv ./docker-compose.yml.out ./docker-compose.yml
+
+scp -o "StrictHostKeyChecking no" -o "UserKnownHostsFile=/dev/null" -P${SSH_PORT} docker-compose.yml ${DO_USER}@${DO_HOST}:~/projects/
+ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile=/dev/null" -p${SSH_PORT} ${DO_USER}@${DO_HOST} 'docker-compose -f ~/projects/docker-compose.yml up -d'
