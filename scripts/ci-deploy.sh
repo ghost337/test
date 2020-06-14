126477b7dc5e6d881abab119fa64dacf0b57da96
Ivan <klimov.i.a@gmail.com>
2020-06-15 01:11:58 +0300

    nineth commit

--- scripts/ci-deploy.sh
+++ scripts/ci-deploy.sh
@@ -7,6 +7,7 @@ echo "hello world 123!"
 envsubst <./docker-compose.yml >./docker-compose.yml.out
 mv ./docker-compose.yml.out ./docker-compose.yml
 
+ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile=/dev/null" -p${SSH_PORT} ${USER}@${INSTANCE} '[ -d ~/projects ] || mkdir -p ~/projects'
 scp -o "StrictHostKeyChecking no" -o "UserKnownHostsFile=/dev/null" -P${SSH_PORT} docker-compose.yml ${DO_USER}@${DO_HOST}:~/projects/
 ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile=/dev/null" -p${SSH_PORT} ${DO_USER}@${DO_HOST} 'docker-compose -f ~/projects/docker-compose.yml pull'
 ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile=/dev/null" -p${SSH_PORT} ${DO_USER}@${DO_HOST} 'docker-compose -f ~/projects/docker-compose.yml up -d --no-build'
