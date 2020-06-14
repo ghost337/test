f43f134733b53fd69636b5b88edbcd163080d1bf
Ivan <klimov.i.a@gmail.com>
2020-06-14 21:20:01 +0300

    third commit

--- scripts/ci-deploy.sh
+++ scripts/ci-deploy.sh
@@ -10,4 +10,5 @@ envsubst <./docker-compose.yml >./docker-compose.yml.out
 mv ./docker-compose.yml.out ./docker-compose.yml
 
 scp -o "StrictHostKeyChecking no" -o "UserKnownHostsFile=/dev/null" -P${SSH_PORT} docker-compose.yml ${DO_USER}@${DO_HOST}:~/projects/
-ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile=/dev/null" -p${SSH_PORT} ${DO_USER}@${DO_HOST} 'docker-compose -f ~/projects/docker-compose.yml up -d'
+ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile=/dev/null" -p${SSH_PORT} ${DO_USER}@${DO_HOST} 'docker-compose -f ~/projects/docker-compose.yml pull'
+ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile=/dev/null" -p${SSH_PORT} ${DO_USER}@${DO_HOST} 'docker-compose -f ~/projects/docker-compose.yml up -d --no-build'
